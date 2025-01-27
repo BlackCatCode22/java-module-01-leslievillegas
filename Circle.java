bc7b6cf1b04eb2afb764ea85c44a8d50e5824d21
leslievillegas <156046351+leslievillegas@users.noreply.github.com>
2025-01-26 16:15:47 -0800

    CircleArea.java

---
+++ Circle.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Circle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter Circle Radius: ");
+        double radius = sc.nextDouble();
+        double area = Math.PI * radius * radius;
+
+        System.out.println("The area of a circle with the radius of " + radius + " is " + area);
+    }
+}
