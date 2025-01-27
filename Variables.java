78dcc50f1605490aa432c5e2a4e6d440542a48b5
leslievillegas <156046351+leslievillegas@users.noreply.github.com>
2025-01-26 16:14:18 -0800

    VarsAndMath.java

---
+++ Variables.java
@@ -0,0 +1,25 @@
+public class Variables {
+    public static void main(String[] args) {
+        // primitive
+//        int hello_world = 9;
+//        double num2 = 5.0;
+//        boolean b = true;
+//        char c = 'h';
+//        String str = "9";
+
+//        int tim = hello_world;
+//        System.out.println(hello_world);
+
+        // double y = 5;
+        // double z = 57;
+        // double u = z / y;
+        // double d = Math.pow(x, y);
+
+        int x = 5;
+        int y = 8;
+        int z = 56;
+        int sum = (x * y) + z;
+
+        System.out.println(sum);
+    }
+}
